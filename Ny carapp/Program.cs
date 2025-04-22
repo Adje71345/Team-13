0eab43434eea90dd2bc1461a96e5927325f0043d
Annette Jensen <annetteduedal@gmail.com>
2025-04-22 14:24:38 +0200

    færdig tirsdag 22 april

--- Ny carapp/Program.cs
+++ Ny carapp/Program.cs
@@ -8,9 +8,9 @@
             fuelCar.StartEngine();
             fuelCar.Refuel(40);
             fuelCar.Drive(100);
-            Console.WriteLine($"Fuelcar Odometer: {fuelCar.Odometer} km. Remaining fuel: {fuelCar.FuelLevel:F2} liters.");
+            PrintCarStatus(fuelCar);
             fuelCar.Drive(700);
-            Console.WriteLine($"Fuelcar Odometer: {fuelCar.Odometer} km. Remaining fuel: {fuelCar.FuelLevel:F2} liters.");
+            PrintCarStatus(fuelCar);
             fuelCar.StopEngine();
             Console.ReadLine();
 
@@ -18,11 +18,63 @@
             electricCar.StartEngine();
             electricCar.Charge(80);
             electricCar.Drive(200);
-            Console.WriteLine($"ElectricCar Odometer: {electricCar.Odometer} km. Remaining battery: {electricCar.BatteryLevel:F2} kWh.");
+            PrintCarStatus(electricCar);
             electricCar.Drive(400);
-            Console.WriteLine($"ElectricCar Odometer: {electricCar.Odometer} km. Remaining battery: {electricCar.BatteryLevel:F2} kWh.");
+            PrintCarStatus(electricCar);
             electricCar.StopEngine();
             Console.ReadLine();
+
+            // Create a Taxi object
+            FuelCar taxi1 = new FuelCar("Volvo", "V70", "TAXI123", 60, 12);
+            ElectricCar taxi2 = new ElectricCar("Nissan", "Leaf", "TAXI456", 40, 6);
+
+            Taxi taxi = new Taxi(taxi2, taxi2.Brand, taxi2.Model, taxi2.LicensePlate, 50, 2, 3);
+            taxi.StartEngine();
+            taxi.StartMeter();
+            taxi.Refuel(40);
+            taxi.Drive(100);
+            PrintCarStatus(taxi);
+            PrintTaxiFare(taxi, 100, 30);
+            taxi.StopMeter();
+            taxi.StopEngine();
+            Console.ReadLine();
         }
+
+        //print liter eller KWh
+        public static void PrintCarStatus(Car car)
+        {
+            if (car is Taxi taxi)
+            {
+                if (taxi.Vehicle is ElectricCar)
+                {
+                    // Hvis taxien bruger en el-bil, vis batteri i stedet for brændstof
+                    Console.WriteLine($"Taxi Odometer: {taxi.Odometer} km. Remaining battery: {((ElectricCar)taxi.Vehicle).BatteryLevel:F2} kWh.");
+                }
+                else
+                {
+                    // Standard tekst for en taxi med brændstofbil
+                    Console.WriteLine($"Taxi Odometer: {taxi.Odometer} km. Remaining fuel: {taxi.FuelLevel:F2} liters.");
+                }
+            }
+            else if (car is FuelCar fuelCar)
+            {
+                Console.WriteLine($"FuelCar Odometer: {fuelCar.Odometer} km. Remaining fuel: {fuelCar.FuelLevel:F2} liters.");
+            }
+            else if (car is ElectricCar electricCar)
+            {
+                Console.WriteLine($"ElectricCar Odometer: {electricCar.Odometer} km. Remaining battery: {electricCar.BatteryLevel:F2} kWh.");
+            }
+            else
+            {
+                Console.WriteLine($"Car Odometer: {car.Odometer} km.");
+            }
+        }
+
+        //printer taxi pris
+        public static void PrintTaxiFare(Taxi taxi, double distance, double time)
+        {
+            Console.WriteLine($"Taxi fare: {taxi.CalculateFare(distance, time):F2} kr.");
+        }
+
     }
 }
--- Ny carapp/Taxi.cs
+++ Ny carapp/Taxi.cs
@@ -8,14 +8,33 @@ namespace Ny_carapp
 {
     internal class Taxi : Car
     {
-        private Car Vehicle { get; set; }
+        public Car Vehicle { get; private set; }
         public double StartPrice { get; set; }
         public double PricePerKm { get; set; }
         public double PricePerMinute { get; set; }
         public bool MeterStarted { get; set; }
-
-        public Taxi(Car vehicle, string brand, string model, string licensePlate, double startPrice, double pricePerKm, double pricePerMinute) : base(vehicle.Brand, model, licensePlate)
+        public double FuelLevel
         {
+            get
+            {
+                if (Vehicle is FuelCar fuelCar)
+                {
+                    return fuelCar.FuelLevel;
+                }
+                else if (Vehicle is ElectricCar electricCar)
+                {
+                    return electricCar.BatteryLevel;
+                }
+                else
+                {
+                    throw new InvalidOperationException("Unknown vehicle type.");
+                }
+            }
+        }
+
+        public Taxi(Car vehicle, string brand, string model, string licensePlate, double startPrice, double pricePerKm, double pricePerMinute) : base(vehicle.Brand, vehicle.Model, vehicle.LicensePlate)
+        {
+            Vehicle = vehicle;
             StartPrice = startPrice;
             PricePerKm = pricePerKm;
             PricePerMinute = pricePerMinute;
@@ -62,6 +81,46 @@ namespace Ny_carapp
         public override bool CanDrive(double distance)
         {
             return Vehicle.CanDrive(distance);
+        }
+
+        public override void UpdateEnergyLevel(double distance)
+        {
+            Vehicle.UpdateEnergyLevel(distance);
+        }
+
+        public override double CalculateConsumption(double distance)
+        {
+            return Vehicle.CalculateConsumption(distance);
+        }
+
+        public override void Drive(double distance)
+        {
+            if (!IsEngineOn)
+            {
+                Console.WriteLine("You need to start the engine before driving.");
+                return;
+            }
+            if (CanDrive(distance) == true)
+            {
+                UpdateEnergyLevel(distance);
+                Odometer += (int)distance;
+            }
+        }
+
+        public void Refuel(double amount)
+        {
+            if (Vehicle is FuelCar fuelCar)
+            {
+                fuelCar.Refuel(amount);
+            }
+            else if (Vehicle is ElectricCar electricCar)
+            {
+                electricCar.Charge(amount);
+            }
+            else
+            {
+                Console.WriteLine("This vehicle cannot be refueled.");
+            }
         }
     }
 }
