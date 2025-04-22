2e9a69a3c40ddcffb3f2aed466b999872a76bb4d
Annette Jensen <annetteduedal@gmail.com>
2025-04-22 13:03:54 +0200

    nu i mappe

---
+++ Ny carapp/FuelCar.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ny_carapp
+{
+    internal class FuelCar : Car
+    {
+        public double FuelLevel { get; set;}
+        public double TankCapacity { get; set; }
+        public double KmPerLiter { get; set; }
+
+        public FuelCar(string brand, string model, string licensePlate, double tankCapacity, double kmPerLiter) : base(brand, model, licensePlate)
+        {
+            TankCapacity = tankCapacity;
+            KmPerLiter = kmPerLiter;
+            FuelLevel = 0;
+        }
+        public void Refuel (double amount)
+        {
+            if (amount < 0)
+            {
+                Console.WriteLine("You cannot refuel a negative amount.");
+                return;
+            }
+            if (FuelLevel + amount > TankCapacity)
+            {
+                Console.WriteLine("You cannot refuel more than the tank capacity.");
+                return;
+            }
+            FuelLevel += amount;
+            Console.WriteLine($"Refueled {amount} liters. Current fuel level: {FuelLevel} liters.");
+        }
+
+        public override bool CanDrive(double distance)
+        {
+            if (FuelLevel <= 0)
+            {
+                Console.WriteLine("You cannot drive without fuel.");
+                return false;
+            }
+            double fuelNeeded = CalculateConsumption(distance);
+            if (fuelNeeded > FuelLevel)
+            {
+                Console.WriteLine("Not enough fuel to drive this distance.");
+                return false;
+            }
+            return true;
+        }
+
+        public override void UpdateEnergyLevel(double distance)
+        {
+            double fuelNeeded = CalculateConsumption(distance);
+            if (fuelNeeded > FuelLevel)
+            {
+                Console.WriteLine("Not enough fuel to drive this distance.");
+                return;
+            }
+            FuelLevel -= fuelNeeded;
+        }
+
+        public override double CalculateConsumption(double distance)
+        {
+            return distance / KmPerLiter;
+        }
+
+
+        public override void Drive (double distance)
+        {
+            if (!IsEngineOn)
+            {
+                Console.WriteLine("You need to start the engine before driving.");
+                return;
+            }
+            if (CanDrive(distance) == true)
+            {
+                UpdateEnergyLevel(distance);
+                Odometer += (int)distance;
+            }  
+        }
+    }
+}
---
+++ Ny carapp/Taxi.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ny_carapp
+{
+    internal class Taxi : Car
+    {
+        private Car Vehicle { get; set; }
+        public double StartPrice { get; set; }
+        public double PricePerKm { get; set; }
+        public double PricePerMinute { get; set; }
+        public bool MeterStarted { get; set; }
+
+        public Taxi(Car vehicle, string brand, string model, string licensePlate, double startPrice, double pricePerKm, double pricePerMinute) : base(vehicle.Brand, model, licensePlate)
+        {
+            StartPrice = startPrice;
+            PricePerKm = pricePerKm;
+            PricePerMinute = pricePerMinute;
+            MeterStarted = false;
+        }
+
+        public void StartMeter()
+        {
+            if (!MeterStarted)
+            {
+                MeterStarted = true;
+                Console.WriteLine("The taxi meter is now running.");
+            }
+            else
+            {
+                Console.WriteLine("The taxi meter is already running.");
+            }
+        }
+
+        public void StopMeter()
+        {
+            if (MeterStarted)
+            {
+                MeterStarted = false;
+                Console.WriteLine("The taxi meter is now stopped.");
+            }
+            else
+            {
+                Console.WriteLine("The taxi meter is already stopped.");
+            }
+        }
+
+        public double CalculateFare(double distance, double minutes)
+        {
+            if (!MeterStarted)
+            {
+                Console.WriteLine("The taxi meter is not running. Please start the meter first.");
+                return 0;
+            }
+            double fare = StartPrice + (PricePerKm * distance) + (PricePerMinute * minutes);
+            return fare;
+        }
+
+        public override bool CanDrive(double distance)
+        {
+            return Vehicle.CanDrive(distance);
+        }
+    }
+}
